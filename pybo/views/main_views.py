ac64d10792ceb126220c502be772901d801602a7
hongdonjoo70 <hongdonjoo1970.gmail.com>
2026-09-10 17:48:23 +0900

    변경

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -7,5 +8,6 @@ def hello_world():
     return 'Hello Pybo!'
 
 @bp.route('/')
-def home_start():
-    return 'Pybo Start'
+def index():
+    question_list = Question.query.order_by(Question.create_date.desc()).all()
+    return render_template('question/question_list.html', question_list=question_list)
